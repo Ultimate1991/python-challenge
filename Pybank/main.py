df5bc954592d14800e959e3ede6a141799dcdaad
Ultimate1991 <57298395+Ultimate1991@users.noreply.github.com>
2020-01-06 11:18:37 -0800

    Robert

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,33 +1,36 @@
 import os
 import csv
-PyBankcsv = os.path.join('c:/Users/rtmcl/Documents/GitHub/python-challenge/Pybank','Resources','budget_data.csv')
-#with open(PyBankcsv, 'r') as csvfile:
-    #csvreader = csv.reader(csvfile, delimiter=",")
-    #csv_header = next(csvreader) 
 
-# Open and read csv
-#with open(Pybankcsv, newline="") as csvfile:
- #   csvreader = csv.reader(csvfile, delimiter=",")
-#NumMonths = sum(1 for row in csvreader)
-#print(NumMonths) 
-# Read in the CSV file
+
+PyBankcsv = os.path.join('c:/Users/rtmcl/Documents/GitHub/python-challenge/Pybank','Resources','budget_data.csv')
+
+
+
+#Create Lists
+profit = []
+monthly_changes = []
+date = []
+
+#Variables
+count = 0
+total_profit = 0
+total_change_profits = 0
+initial_profit = 0
 
 with open(PyBankcsv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
 
       
-    #for row in csvreader:
-        #print(row)
+    for row in csvreader:
+        print(row)
+            # Conducting the ask
+    for row in csvreader:    
+      # Use count to count the number months in this dataset
+      count = count + 1 
 
-#emply lists for month and revenue data
-months = []
-revenue = []
+      # Will need it when collecting the greatest increase and decrease in profits
+      date.append(row[0])
 
 
-for row in csvread:
-months.append(row[0])
-revenue.append(int(row[1]))
 
-#find total months
-total_months = len(months)
--- Pypoll/main.py
+++ Pypoll/main.py
@@ -1,14 +1,15 @@
 import os
 import csv
 
-PyPollcsv = os.path.join('c:/Users/rtmcl/Documents/GitHub/python-challenge/Pypoll','Resources','election_data.csv')
-#Variables
+# path to data file
+budget_data_csv = os.path.join('election_data.csv')
+
+#variables
 total_votes = 0
 candidates_unique = []
 candidate_vote_count = []
 
-#read the csv file
-with open(election_data, newline="") as csvfile:
+with open(budget_data_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
 
@@ -25,4 +26,33 @@ with open(election_data, newline="") as csvfile:
             #if candidate was not found in candidates_unique list then append to list and add 1 to vote count
             candidates_unique.append(candidate_in)
             candidate_vote_count.append(1)
+
+
+#import os
+#import csv
+
+#PyPollcsv = os.path.join,'Resources','election_data.csv')
+#Variables
+#total_votes = 0
+#candidates_unique = []
+#candidate_vote_count = []
+
+#read the csv file
+#with open(election_data, newline="") as csvfile:
+    #csvreader = csv.reader(csvfile, delimiter=",")
+    #csv_header = next(csvreader)
+
+    #for row in csvreader:
+        #This is the total votes cast, just count rows
+        #total_votes += 1
+        #read in the candidate from column 3 of csv
+        #candidate_in = (row[2])
+        #if candidate alreaady in list then locate the candidate by index # and increment the vote count by 1
+        #if candidate_in in candidates_unique:
+            #candidate_index = candidates_unique.index(candidate_in)
+            #candidate_vote_count[candidate_index] = candidate_vote_count[candidate_index] + 1
+        #else:
+            #if candidate was not found in candidates_unique list then append to list and add 1 to vote count
+            #candidates_unique.append(candidate_in)
+            #candidate_vote_count.append(1)
 
