b622b9ed092f407435ea62b16f72320b639821a5
Ultimate1991 <57298395+Ultimate1991@users.noreply.github.com>
2020-01-06 14:07:19 -0800

    Robert

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -3,29 +3,64 @@ import csv
 
 csvpath = Pypollcsv = os.path.join('c:/Users/rtmcl/Documents/GitHub/python-challenge/Pypoll','Resources','election_data.csv')
 
-#variables
-total_votes = 0
-candidates_unique = []
-candidate_vote_count = []
-
-with open(election_data.csv, newline="") as csvfile:
+# Open and read the CSV
+with open(csvpath, newline="") as csvfile:
+    #print(csvreader)
+    
+    # Read header row, print it, set it aside
     csvreader = csv.reader(csvfile, delimiter=",")
-    csv_header = next(csvreader)
+    csv_header = next(csvfile)
+    #print(f"Header: {csv_header}")
+ 
+    #Declare variables as empty dictionaries and lists
+    Candidates = {}
+    Count = 0
+    Votes_Cast = 0
+    percent_of_votes = 0
+    Most_Votes = 0
+    Most_Voted = ""
+    
 
+    
     for row in csvreader:
-        #Total votes cast
-        total_votes += 1
-        #read in the candidate from column 3 of csv
-        candidate_in = (row[2])
-        #if candidate alreaady in list then locate the candidate by index # and increment the vote count by 1
-        if candidate_in in candidates_unique:
-            candidate_index = candidates_unique.index(candidate_in)
-            candidate_vote_count[candidate_index] = candidate_vote_count[candidate_index] + 1
+        
+        # Count the total number of votes cast
+        candidate = row[2]
+        Count += 1
+        if candidate in Candidates.keys():
+            Candidates[candidate] += 1
         else:
-            #if candidate was not found in candidates_unique list then append to list and add 1 to vote count
-            candidates_unique.append(candidate_in)
-            candidate_vote_count.append(1)
-
+            Candidates[candidate] = 1
+        #print(Candidates)
+    
+    
+    # Print Statements
+    print("Election Results")
+    print("-------------------------------")
+    print(f"Total Votes: {Count}")
+    print("-------------------------------")
+    
+            
+    #total number of votes for each candidate
+    for candidate in Candidates:
+        Votes_Cast += Candidates[candidate]
+    
+        # percent of votes for each candidate
+        percent_of_votes = (Candidates[candidate])/(Count) * 100
+        print(f"{candidate}: {int(percent_of_votes)}% {Votes_Cast}")
+        
+        if Candidates[candidate] > Most_Votes:
+            Most_Voted = candidate
+            Most_Votes = Candidates[candidate]
+        
+        
+    
+    # The winner of the election based on popular vote.
+    print("-------------------------------")
+    
+    print(f"Winner: {Most_Voted}")
+    
+    print("-------------------------------")
 
 
 
